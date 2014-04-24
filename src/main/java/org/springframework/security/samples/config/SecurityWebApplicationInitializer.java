2e83285129f0435001b8b276c7090ac10c185c81
elton-oliveira <elton.pmp@gmail.com>
2014-04-23 21:20:30 -0300

    comment

--- src/main/java/org/springframework/security/samples/config/SecurityWebApplicationInitializer.java
+++ src/main/java/org/springframework/security/samples/config/SecurityWebApplicationInitializer.java
@@ -4,8 +4,13 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
 
 /**
  * 
- * Automatically register the springSecurityFilterChain Filter for every URL in your application
- *
+ * Automatically register the springSecurityFilterChain Filter for every URL in
+ * your application.
+ * 
+ * The way in which we use AbstractSecurityWebApplicationInitializer differs
+ * depending on if we are already using Spring or if Spring Security is the only
+ * Spring component in our application.
+ * 
  */
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
